03470167e18d68d0bd1ac423e1e83787815ee01c
Olha_Chumbash <olyachumbash@gmail.com>
2024-09-08 23:46:24 +0200

    Initial commit to new repository

--- src/js/script.js
+++ src/js/script.js
@@ -521,6 +521,7 @@ $(document).ready(function () {
    $(window).scroll(function () {
       if ($(this).scrollTop() > 400) {
          $(".scrollup").fadeIn();
+        
       } else {
          $(".scrollup").fadeOut();
       }
